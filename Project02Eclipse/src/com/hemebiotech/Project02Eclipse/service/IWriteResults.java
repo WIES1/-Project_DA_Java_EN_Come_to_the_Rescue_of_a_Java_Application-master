678e18dc8e1a98415a175c08edd038775b49f7e2
WIES1 <mywiesel@gmail.com>
2021-03-26 16:11:40 +0100

    refactor

--- Project02Eclipse/src/com/hemebiotech/Project02Eclipse/service/IWriteResults.java
+++ Project02Eclipse/src/com/hemebiotech/Project02Eclipse/service/IWriteResults.java
@@ -3,6 +3,9 @@ package com.hemebiotech.Project02Eclipse.service;
 import java.io.IOException;
 import java.util.Map;
 
+/**
+ *
+ */
 public interface IWriteResults {
-    void writeResultsToFile(Map<String, Integer> symptoms) throws IOException;  // le comportement est impose
+    void writeResultsToFile(Map<String, Integer> symptoms) throws IOException;
 }
