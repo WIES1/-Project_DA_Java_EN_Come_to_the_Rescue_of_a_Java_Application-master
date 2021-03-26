1c46c7c5857d6c378861c156c42cb1cd6898f719
WIES1 <76532252+WIES1@users.noreply.github.com>
2021-03-26 16:07:36 +0100

    Update IWriteResults.java

--- Project02Eclipse/src/com/hemebiotech/Project02Eclipse/service/IWriteResults.java
+++ Project02Eclipse/src/com/hemebiotech/Project02Eclipse/service/IWriteResults.java
@@ -4,5 +4,5 @@ import java.io.IOException;
 import java.util.Map;
 
 public interface IWriteResults {
-    void writeResultsToFile(Map<String, Integer> symptoms) throws IOException;  // le comportement est impose
+    void writeResultsToFile(Map<String, Integer> symptoms) throws IOException;  
 }
