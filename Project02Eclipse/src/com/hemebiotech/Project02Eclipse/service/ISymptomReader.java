92263fbc28ec4f02bb9688a48b26a659a67269a7
WIES1 <mywiesel@gmail.com>
2021-03-28 22:39:35 +0100

    Rename usages of 'GetSymptoms' to 'getSymptoms'

--- Project02Eclipse/src/com/hemebiotech/Project02Eclipse/service/ISymptomReader.java
+++ Project02Eclipse/src/com/hemebiotech/Project02Eclipse/service/ISymptomReader.java
@@ -13,8 +13,8 @@ import java.util.List;
 public interface ISymptomReader {
 	/**
 	 * If no data is available, return an empty List
-	 * 
+	 *
 	 * @return a raw listing of all Symptoms obtained from a data source, duplicates are possible/probable
 	 */
-	List<String> GetSymptoms ();
+	List<String> getSymptoms();
 }
